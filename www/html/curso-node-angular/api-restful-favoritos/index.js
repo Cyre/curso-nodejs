dc81f2993ca120e4230c27ddd1f2d037ea5e8f27
Cyre <kaai.cyre@gmail.com>
2018-04-05 20:34:07 +0100

    Introduction to NodeJS
    Part 9 (arrow functions)

--- www/html/curso-node-angular/api-restful-favoritos/index.js
+++ www/html/curso-node-angular/api-restful-favoritos/index.js
@@ -7,9 +7,27 @@ var app = express();
 var port = process.env.PORT || 3678;
 
 app.use(bodyParser.urlencoded({extended:false}));
-app.use(bodyParser.json())
+app.use(bodyParser.json());
+
+// Uso de rutas.
+// Utiliza el metodo get de http
+app.get('/prueba/:nombre?', function(request, response) {
+	//                   ^<-- Indica que el parametro es opcional
+	//Primer parametro de la funcion es el request
+	//Segundo parametro es la respuesta de la funcion
+	
+	if (request.params.nombre) {
+		var nombre = request.params.nombre;
+	} else {
+		var nombre = "SIN NOMBRE";
+	}
+	response.status(200).send({
+		data: [2,3,4],
+		texto: "Hola Mundo con NodeJS y EXPRESS. " + nombre,
+		});
+});
+
 
 app.listen(port, function(){
-	console.log(`API REST FAVORITOS funcionando en http://localhost:${port}`);
-	
+	console.log(`API REST FAVORITOS funcionando en http://localhost:${port}`);	
 });
